4a1d6e797aacdb0b16ee6316edfe655083a46e7c
Angelos Zaros <129210112+AngelosZar@users.noreply.github.com>
2024-09-01 16:05:48 +0200

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bd4d416b-f02b-4c22-a8d7-d97a73b9f151';
+export const MODAL_CLOSE_SEC = 2.5;
