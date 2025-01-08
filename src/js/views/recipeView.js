ace2ba42530038f37942b7ef2184b295a574eb98
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-01-08 12:08:31 +0100

    fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,9 +2,9 @@ import View from './view.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-// import { fraction } from 'fractional';
-import { Fraction } from 'fractional';
-
+import Fractional from 'fractional'; //
+const Fraction = Fractional.Fraction; //
+// console.log('Fraction imported:', Fraction);
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
